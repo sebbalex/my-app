e20d68048665a521da9873efdfe7d7655864e546
Alessandro Sebastiani <sebbalex@gmail.com>
2020-05-21 18:15:09 +0200

    using materialui instead of design-react-kit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from "design-react-kit";
-
+import Button from '@material-ui/core/Button';
 
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import "typeface-titillium-web";
@@ -18,11 +17,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button color="primary" icon={false} tag="button">
-          Save
-        </Button>{" "}
-        <Button color="secondary" icon={false} tag="button">
-          Reset
+        <Button variant="contained" color="primary">
+          Hello World
         </Button>
         <a
           className="App-link"
