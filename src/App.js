426fd07a8f98e82a43db2ca1e779873228206eb9
Alessandro Sebastiani <sebbalex@gmail.com>
2020-05-21 16:54:22 +0200

    bump back to design-react-kit:3.1.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "design-react-kit";
+
+
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "typeface-titillium-web";
+import "typeface-roboto-mono";
+import "typeface-lora";
+
 
 function App() {
   return (
@@ -10,6 +18,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="primary" icon={false} tag="button">
+          Save
+        </Button>{" "}
+        <Button color="secondary" icon={false} tag="button">
+          Reset
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
